fb2044c95f086770d4c7b9fb0012ffc9d50068e1
Bruce Hung <brucewmhung@gmail.com>
2025-11-10 17:18:50 +0800

    install typescript-eslint/plugin/parser & move publicRoutes out of route.ts

---
+++ app/_lib/routes.ts
@@ -0,0 +1,5 @@
+export const publicRoutes = ['/'];
+export const parklistPrefix = ['/parklist'];
+export const authRoutes = ['/login', '/register'];
+export const apiAuthPrefix = ['/api/auth'];
+export const DEFAULT_LOGIN_REDIRECT = '/';
